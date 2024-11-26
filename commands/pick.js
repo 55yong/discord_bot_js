c57412c5181d6871fca090b7bf697c86d2c29f64
55yong <dhdydwns988@gmail.com>
2024-11-26 16:41:20 +0900

    Modify: 영어로 변경

--- commands/pick.js
+++ commands/pick.js
@@ -3,23 +3,27 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   deleted: false,
   data: new SlashCommandBuilder()
-    .setName("추첨")
-    .setDescription("음성 채널에 있는 플레이어 중 한 명을 무작위로 추첨합니다.")
+    .setName("pick")
+    .setDescription(
+      "Randomly selects one or more players from the voice channel."
+    )
     .addIntegerOption((option) =>
       option
-        .setName("인원수")
-        .setDescription("추첨할 인원수를 입력해주세요.")
+        .setName("count")
+        .setDescription("Enter the number of players to pick.")
         .setRequired(true)
         .setMaxValue(3)
     ),
   run: ({ interaction }) => {
-    const cnt = interaction.options.get("인원수").value;
+    const cnt = interaction.options.get("count").value;
     try {
       const members = Array.from(
         interaction.member.voice.channel.members.values()
       );
       if (cnt > members.length) {
-        interaction.reply("추첨할 인원수가 음성 채널의 멤버 수보다 많습니다.");
+        interaction.reply(
+          "The number of players to pick is greater than the number of members in the voice channel."
+        );
         return;
       }
 
@@ -32,9 +36,11 @@ module.exports = {
       const winningNames = winning_members
         .map((member) => `<@${member.user.id}>`)
         .join(", ");
-      interaction.reply(`:tada: 당첨 :trophy: ${winningNames}`);
+      interaction.reply(`:tada: Winner :trophy: ${winningNames}`);
     } catch (e) {
-      interaction.reply("음성 채널에 접속 후 사용해주세요.");
+      interaction.reply(
+        "Please join a voice channel before using this command."
+      );
       console.log(e);
     }
   },
