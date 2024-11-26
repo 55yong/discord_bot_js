3951d2506ce28939e335cdc86020347f71673d96
55yong <dhdydwns988@gmail.com>
2024-11-26 16:41:40 +0900

    Modify: lotto -> draw로 변경

--- commands/draw.js
+++ commands/draw.js
@@ -3,22 +3,22 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   deleted: false,
   data: new SlashCommandBuilder()
-    .setName("뽑기")
-    .setDescription("입력한 최대 인원수 중 한 명을 뽑습니다.")
+    .setName("draw")
+    .setDescription("Randomly selects one number up to the specified maximum.")
     .addIntegerOption((option) =>
       option
-        .setName("최대인원수")
-        .setDescription("최대 인원수를 입력해주세요.")
+        .setName("max")
+        .setDescription("Enter the maximum number.")
         .setRequired(true)
     ),
   run: ({ interaction }) => {
-    const cnt = interaction.options.get("최대인원수").value;
+    const cnt = interaction.options.get("max").value;
     try {
       const rand = Math.floor(Math.random() * cnt + 1);
 
-      interaction.reply(`${rand}번`);
+      interaction.reply(`Selected number: ${rand}`);
     } catch (e) {
-      interaction.reply(`${e}`);
+      interaction.reply(`Error: ${e}`);
     }
   },
   managerOnly: false,
